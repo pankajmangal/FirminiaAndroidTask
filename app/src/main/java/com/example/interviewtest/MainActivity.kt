a9048fc8fdfe56fec99fe1e126a970869dd95d9c
Pankaj Mangal <mangal.pankaj5@gmail.com>
2020-11-28 23:15:18 +0530

    add viewmodel statehandle libs but implementation pending...

--- app/src/main/java/com/example/interviewtest/MainActivity.kt
+++ app/src/main/java/com/example/interviewtest/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.interviewtest
 
-import android.content.DialogInterface
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
